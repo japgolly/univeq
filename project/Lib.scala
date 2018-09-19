79c3285fb24c22bf0b45e9028e0d1fc6d0f0e663
David Barri <japgolly@gmail.com>
2018-09-19 12:25:56 +1000

    sbt-release didn't work

--- project/Lib.scala
+++ project/Lib.scala
@@ -67,5 +67,14 @@ object Lib {
     )
 
   def preventPublication: PE =
-    _.settings(skip in publish := true)
+    _.settings(
+      skip in publish    := true,
+      publish            := (),
+      publishLocal       := (),
+      publishSigned      := (),
+      publishLocalSigned := (),
+      publishArtifact    := false,
+      publishTo          := Some(Resolver.file("Unused transient repository", target.value / "fakepublish")),
+      packagedArtifacts  := Map.empty)
+    // .disablePlugins(plugins.IvyPlugin)
 }
