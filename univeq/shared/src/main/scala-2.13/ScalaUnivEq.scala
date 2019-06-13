7d47f5f8224d03a4406bf1477dc8c1c4947c4acd
David Barri <japgolly@gmail.com>
2019-06-14 00:19:28 +1000

    Regress...

---
+++ univeq/shared/src/main/scala-2.13/ScalaUnivEq.scala
@@ -0,0 +1,60 @@
+package japgolly.univeq
+
+import scala.collection.{immutable => sci}
+import scala.concurrent.{duration => sd}
+import scala.collection._
+import UnivEq.force
+
+trait ScalaUnivEq {
+
+  // Scala Predef
+  @inline implicit def univEqBigDecimal                        : UnivEq[BigDecimal  ] = force
+  @inline implicit def univEqBigInt                            : UnivEq[BigInt      ] = force
+  @inline implicit def univEqEither    [A: UnivEq, B: UnivEq]  : UnivEq[Either[A, B]] = force
+  @inline implicit def univEqEitherL   [A: UnivEq, B        ]  : UnivEq[Left  [A, B]] = force
+  @inline implicit def univEqEitherR   [A        , B: UnivEq]  : UnivEq[Right [A, B]] = force
+  @inline implicit def univEqEnum      [A <: Enumeration#Value]: UnivEq[A]            = force
+  @inline implicit def univEqLazyList  [A: UnivEq]             : UnivEq[LazyList [A]] = force
+  @inline implicit def univEqList      [A: UnivEq]             : UnivEq[List     [A]] = force
+  @inline implicit def univEqMap       [A: UnivEq, B: UnivEq]  : UnivEq[Map   [A, B]] = force
+  @inline implicit def univEqNone                              : UnivEq[None.type   ] = force
+  @inline implicit def univEqOption    [A: UnivEq]             : UnivEq[Option   [A]] = force
+  @inline implicit def univEqRange                             : UnivEq[Range       ] = force
+  @inline implicit def univEqSeq       [A: UnivEq]             : UnivEq[Seq      [A]] = force
+  @inline implicit def univEqSet       [A: UnivEq]             : UnivEq[Set      [A]] = force
+  @inline implicit def univEqSome      [A: UnivEq]             : UnivEq[Some     [A]] = force
+  @inline implicit def univEqStream    [A: UnivEq]             : UnivEq[Stream   [A]] = force
+  @inline implicit def univEqVector    [A: UnivEq]             : UnivEq[Vector   [A]] = force
+
+  // scala.collection.immutable
+  @inline implicit def univEqSciArraySeq  [A: UnivEq           ]: UnivEq[sci.ArraySeq  [A]   ] = force
+  @inline implicit def univEqSciBitSet    [A: UnivEq           ]: UnivEq[sci.BitSet          ] = force
+  @inline implicit def univEqSciHashMap   [A: UnivEq, B: UnivEq]: UnivEq[sci.HashMap   [A, B]] = force
+  @inline implicit def univEqSciHashSet   [A: UnivEq           ]: UnivEq[sci.HashSet   [A]   ] = force
+  @inline implicit def univEqSciIndexedSeq[A: UnivEq           ]: UnivEq[sci.IndexedSeq[A]   ] = force
+  @inline implicit def univEqSciIntMap    [A: UnivEq           ]: UnivEq[sci.IntMap    [A]   ] = force
+  @inline implicit def univEqSciLinearSeq [A: UnivEq           ]: UnivEq[sci.LinearSeq [A]   ] = force
+  @inline implicit def univEqSciListMap   [A: UnivEq, B: UnivEq]: UnivEq[sci.ListMap   [A, B]] = force
+  @inline implicit def univEqSciListSet   [A: UnivEq           ]: UnivEq[sci.ListSet   [A]   ] = force
+  @inline implicit def univEqSciLongMap   [A: UnivEq           ]: UnivEq[sci.LongMap   [A]   ] = force
+  @inline implicit def univEqSciQueue     [A: UnivEq           ]: UnivEq[sci.Queue     [A]   ] = force
+  @inline implicit def univEqSciSortedMap [A: UnivEq, B: UnivEq]: UnivEq[sci.SortedMap [A, B]] = force
+  @inline implicit def univEqSciSortedSet [A: UnivEq           ]: UnivEq[sci.SortedSet [A]   ] = force
+  @inline implicit def univEqSciTreeMap   [A: UnivEq, B: UnivEq]: UnivEq[sci.TreeMap   [A, B]] = force
+  @inline implicit def univEqSciTreeSeqMap[A: UnivEq, B: UnivEq]: UnivEq[sci.TreeSeqMap[A, B]] = force
+  @inline implicit def univEqSciTreeSet   [A: UnivEq           ]: UnivEq[sci.TreeSet   [A]   ] = force
+  @inline implicit def univEqSciVectorMap [A: UnivEq, B: UnivEq]: UnivEq[sci.VectorMap [A, B]] = force
+
+  // scala.concurrent.duration
+  @inline implicit def univEqSDuration      : UnivEq[sd.Duration      ] = force
+  @inline implicit def univEqSFiniteDuration: UnivEq[sd.FiniteDuration] = force
+
+  // ===================================================================================================================
+
+  @inline def emptyMap       [K: UnivEq, V] = Map.empty[K, V]
+  @inline def emptySet       [A: UnivEq]    = Set.empty[A]
+  @inline def emptyMutableSet[A: UnivEq]    = collection.mutable.Set.empty[A]
+  @inline def setBuilder     [A: UnivEq]    = Set.newBuilder[A]
+
+  @inline def toSet[A: UnivEq](as: IterableOnce[A]): Set[A] = as.iterator.toSet
+}
--- univeq/shared/src/main/scala/japgolly/univeq/UnivEq.scala
+++ univeq/shared/src/main/scala/japgolly/univeq/UnivEq.scala
@@ -2,9 +2,6 @@ package japgolly.univeq
 
 import java.{lang => jl}
 import java.{time => jt}
-import scala.collection.{immutable => sci}
-import scala.concurrent.{duration => sd}
-import scala.collection._
 
 /**
  * Universal equality.
@@ -14,7 +11,7 @@ trait UnivEq[@specialized A] {
     a == b
 }
 
-object UnivEq extends PlatformUnivEq {
+object UnivEq extends ScalaUnivEq with PlatformUnivEq {
 
   @inline def apply[A](implicit proof: UnivEq[A]): UnivEq[A] =
     proof
@@ -60,48 +57,6 @@ object UnivEq extends PlatformUnivEq {
   @inline implicit def univEqTuple21[A:UnivEq, B:UnivEq, C:UnivEq, D:UnivEq, E:UnivEq, F:UnivEq, G:UnivEq, H:UnivEq, I:UnivEq, J:UnivEq, K:UnivEq, L:UnivEq, M:UnivEq, N:UnivEq, O:UnivEq, P:UnivEq, Q:UnivEq, R:UnivEq, S:UnivEq, T:UnivEq, U:UnivEq]: UnivEq[(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U)] = force
   @inline implicit def univEqTuple22[A:UnivEq, B:UnivEq, C:UnivEq, D:UnivEq, E:UnivEq, F:UnivEq, G:UnivEq, H:UnivEq, I:UnivEq, J:UnivEq, K:UnivEq, L:UnivEq, M:UnivEq, N:UnivEq, O:UnivEq, P:UnivEq, Q:UnivEq, R:UnivEq, S:UnivEq, T:UnivEq, U:UnivEq, V:UnivEq]: UnivEq[(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V)] = force
 
-  // Scala Predef
-  @inline implicit def univEqBigDecimal                        : UnivEq[BigDecimal  ] = force
-  @inline implicit def univEqBigInt                            : UnivEq[BigInt      ] = force
-  @inline implicit def univEqEither    [A: UnivEq, B: UnivEq]  : UnivEq[Either[A, B]] = force
-  @inline implicit def univEqEitherL   [A: UnivEq, B        ]  : UnivEq[Left  [A, B]] = force
-  @inline implicit def univEqEitherR   [A        , B: UnivEq]  : UnivEq[Right [A, B]] = force
-  @inline implicit def univEqEnum      [A <: Enumeration#Value]: UnivEq[A]            = force
-  @inline implicit def univEqLazyList  [A: UnivEq]             : UnivEq[LazyList [A]] = force
-  @inline implicit def univEqList      [A: UnivEq]             : UnivEq[List     [A]] = force
-  @inline implicit def univEqMap       [A: UnivEq, B: UnivEq]  : UnivEq[Map   [A, B]] = force
-  @inline implicit def univEqNone                              : UnivEq[None.type   ] = force
-  @inline implicit def univEqOption    [A: UnivEq]             : UnivEq[Option   [A]] = force
-  @inline implicit def univEqRange                             : UnivEq[Range       ] = force
-  @inline implicit def univEqSeq       [A: UnivEq]             : UnivEq[Seq      [A]] = force
-  @inline implicit def univEqSet       [A: UnivEq]             : UnivEq[Set      [A]] = force
-  @inline implicit def univEqSome      [A: UnivEq]             : UnivEq[Some     [A]] = force
-  @inline implicit def univEqStream    [A: UnivEq]             : UnivEq[Stream   [A]] = force
-  @inline implicit def univEqVector    [A: UnivEq]             : UnivEq[Vector   [A]] = force
-
-  // scala.collection.immutable
-  @inline implicit def univEqSciArraySeq  [A: UnivEq           ]: UnivEq[sci.ArraySeq  [A]   ] = force
-  @inline implicit def univEqSciBitSet    [A: UnivEq           ]: UnivEq[sci.BitSet          ] = force
-  @inline implicit def univEqSciHashMap   [A: UnivEq, B: UnivEq]: UnivEq[sci.HashMap   [A, B]] = force
-  @inline implicit def univEqSciHashSet   [A: UnivEq           ]: UnivEq[sci.HashSet   [A]   ] = force
-  @inline implicit def univEqSciIndexedSeq[A: UnivEq           ]: UnivEq[sci.IndexedSeq[A]   ] = force
-  @inline implicit def univEqSciIntMap    [A: UnivEq           ]: UnivEq[sci.IntMap    [A]   ] = force
-  @inline implicit def univEqSciLinearSeq [A: UnivEq           ]: UnivEq[sci.LinearSeq [A]   ] = force
-  @inline implicit def univEqSciListMap   [A: UnivEq, B: UnivEq]: UnivEq[sci.ListMap   [A, B]] = force
-  @inline implicit def univEqSciListSet   [A: UnivEq           ]: UnivEq[sci.ListSet   [A]   ] = force
-  @inline implicit def univEqSciLongMap   [A: UnivEq           ]: UnivEq[sci.LongMap   [A]   ] = force
-  @inline implicit def univEqSciQueue     [A: UnivEq           ]: UnivEq[sci.Queue     [A]   ] = force
-  @inline implicit def univEqSciSortedMap [A: UnivEq, B: UnivEq]: UnivEq[sci.SortedMap [A, B]] = force
-  @inline implicit def univEqSciSortedSet [A: UnivEq           ]: UnivEq[sci.SortedSet [A]   ] = force
-  @inline implicit def univEqSciTreeMap   [A: UnivEq, B: UnivEq]: UnivEq[sci.TreeMap   [A, B]] = force
-  @inline implicit def univEqSciTreeSeqMap[A: UnivEq, B: UnivEq]: UnivEq[sci.TreeSeqMap[A, B]] = force
-  @inline implicit def univEqSciTreeSet   [A: UnivEq           ]: UnivEq[sci.TreeSet   [A]   ] = force
-  @inline implicit def univEqSciVectorMap [A: UnivEq, B: UnivEq]: UnivEq[sci.VectorMap [A, B]] = force
-
-  // scala.concurrent.duration
-  @inline implicit def univEqSDuration      : UnivEq[sd.Duration      ] = force
-  @inline implicit def univEqSFiniteDuration: UnivEq[sd.FiniteDuration] = force
-
   // Java
   @inline implicit def univEqClass  [A]            : UnivEq[Class  [A]] = force
   @inline implicit def univEqClass_                : UnivEq[Class  [_]] = force
@@ -140,13 +95,4 @@ object UnivEq extends PlatformUnivEq {
     @inline implicit def autoDeriveUnivEq[A <: AnyRef]: UnivEq[A] =
       macro macros.UnivEqMacros.deriveQuiet[A]
   }
-
-  // ===================================================================================================================
-
-  @inline def emptyMap       [K: UnivEq, V] = Map.empty[K, V]
-  @inline def emptySet       [A: UnivEq]    = Set.empty[A]
-  @inline def emptyMutableSet[A: UnivEq]    = collection.mutable.Set.empty[A]
-  @inline def setBuilder     [A: UnivEq]    = Set.newBuilder[A]
-
-  @inline def toSet[A: UnivEq](as: IterableOnce[A]): Set[A] = as.iterator.toSet
 }
