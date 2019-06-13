994861ff26256b4c9ab91620bf8c9a84c57ee3b4
David Barri <japgolly@gmail.com>
2019-06-13 22:48:34 +1000

    Fix utest
    find . -path '*/test/*.scala' -exec perl -pi -e '
      s/^( *)'"'"'([^ ]+)\{/\1"\2" - {/;
      s/^( *)'"'"'([^ ]+)( +)(?:- *)?\{/\1"\2"\3- {/;
      s/^( *)'"'"'([^ ]+)( +)-/\1"\2"\3-/;
    ' {} +

--- univeq/shared/src/test/scala/japgolly/univeq/RuntimeTest.scala
+++ univeq/shared/src/test/scala/japgolly/univeq/RuntimeTest.scala
@@ -34,50 +34,50 @@ object RuntimeTest extends TestSuite {
   }
 
   override def tests = Tests {
-    'scala {
-      'unit    - assertId(())
+    "scala" - {
+      "unit"    - assertId(())
 
-      'long    - assertPass[Long   ](4L, 5L)
-      'int     - assertPass[Int    ](3, 5)
-      'short   - assertPass[Short  ](2, 7)
-      'byte    - assertPass[Byte   ](1, 3)
+      "long"    - assertPass[Long   ](4L, 5L)
+      "int"     - assertPass[Int    ](3, 5)
+      "short"   - assertPass[Short  ](2, 7)
+      "byte"    - assertPass[Byte   ](1, 3)
 
-      'char    - assertPass('a', 'H')
-      'boolean - assertPass(true, false)
-      'string  - assertPass("x", "")
+      "char"    - assertPass('a', 'H')
+      "boolean" - assertPass(true, false)
+      "string"  - assertPass("x", "")
 
-      'option  - assertPass[Option[Int]](Some(2), None, Some(4))
+      "option"  - assertPass[Option[Int]](Some(2), None, Some(4))
 
-      'enum    - assertPass(Colours.Amber, Colours.Green, Colours.Red)
+      "enum"    - assertPass(Colours.Amber, Colours.Green, Colours.Red)
     }
 
-    'java{
-      'class {
+    "java" - {
+      "class" - {
         compileError("clsI ==* clsL")
         assert(clsI == clsI)
         assertId(clsL)
       }
 
-      'class_ - assertPass[Class[_]](clsI, clsL)
+      "class_" - assertPass[Class[_]](clsI, clsL)
     }
 
-    'derived - assertPass(Wrap(2), Wrap(3))
+    "derived" - assertPass(Wrap(2), Wrap(3))
 
-    'ops {
+    "ops" - {
       compileError("3 ==* false")
       assert(3 ==* 3)
       assert(3 !=* 4)
     }
 
-    'force {
+    "force" - {
       def test[A](a: A, b: A): Unit = {
         implicit def f = UnivEq.force[A]
         assert(a ==* a)
         assert(a !=* b)
       }
-      'anyref - test(I(1), I(2))
-      'int    - test(1, 2)
-      'bool   - test(true, false)
+      "anyref" - test(I(1), I(2))
+      "int"    - test(1, 2)
+      "bool"   - test(true, false)
     }
   }
 }
