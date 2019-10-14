916326b396287de00edf0ec883847b74c4204746
Scala Steward <me@scala-steward.org>
2019-10-14 05:30:43 +0200

    Update scalaz-core to 7.2.29

--- project/Build.scala
+++ project/Build.scala
@@ -20,7 +20,7 @@ object UnivEqBuild {
     final val MTest           = "0.6.9"
     final val Scala212        = "2.12.8"
     final val Scala213        = "2.13.0"
-    final val Scalaz          = "7.2.28"
+    final val Scalaz          = "7.2.29"
     final val ScalaCollCompat = "2.1.2"
     final val ScalaJsDom      = "0.9.7"
   }
