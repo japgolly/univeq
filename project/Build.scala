5204081cf2390b27665b14006e8bc3374a818333
Scala Steward <me@scala-steward.org>
2019-12-13 01:19:01 +0100

    Update scala-collection-compat to 2.1.3

--- project/Build.scala
+++ project/Build.scala
@@ -21,7 +21,7 @@ object UnivEqBuild {
     final val Scala212        = "2.12.8"
     final val Scala213        = "2.13.0"
     final val Scalaz          = "7.2.28"
-    final val ScalaCollCompat = "2.1.2"
+    final val ScalaCollCompat = "2.1.3"
     final val ScalaJsDom      = "0.9.7"
   }
 
