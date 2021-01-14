287c5f133b6ac94209b549390af9323b0a5ad9e2
Scala Steward <me@scala-steward.org>
2021-01-14 22:44:27 +0100

    Update scalaz-core to 7.3.2

--- project/Build.scala
+++ project/Build.scala
@@ -21,7 +21,7 @@ object UnivEqBuild {
     val MTest           = "0.7.5"
     val Scala212        = "2.12.11"
     val Scala213        = "2.13.4"
-    val Scalaz          = "7.2.30"
+    val Scalaz          = "7.3.2"
     val ScalaCollCompat = "2.3.2"
     val ScalaJsDom      = "1.1.0"
   }
