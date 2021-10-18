11c22d3a5c82fb60fa9b151e473c9d7430f3fecf
David Barri <japgolly@gmail.com>
2021-10-18 12:33:30 +1100

    Upgrade scalajs-dom

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val cats       = "2.6.1"
     val scala2     = "2.13.6"
     val scala3     = "3.0.1"
-    val scalaJsDom = "1.1.0"
+    val scalaJsDom = "2.0.0-RC1"
 
     // Internal
     val utest      = "0.7.10"
@@ -19,7 +19,7 @@ object Dependencies {
   object Dep {
     val cats          = Def.setting("org.typelevel"  %%% "cats-core"      % Ver.cats)
     val scalaCompiler = Def.setting("org.scala-lang"   % "scala-compiler" % scalaVersion.value)
-    val scalaJsDom    = Def.setting("org.scala-js"   %%% "scalajs-dom"    % Ver.scalaJsDom cross CrossVersion.for3Use2_13)
+    val scalaJsDom    = Def.setting("org.scala-js"   %%% "scalajs-dom"    % Ver.scalaJsDom)
     val utest         = Def.setting("com.lihaoyi"    %%% "utest"          % Ver.utest)
   }
 }
